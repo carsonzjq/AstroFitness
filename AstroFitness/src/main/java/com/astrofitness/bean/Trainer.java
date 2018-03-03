0602231842b6d8bec4f10a6f358ef8375c585117
Petar <zigic@outlook.com>
2018-03-03 11:05:04 -0500

    added hibernate functionality, some hibernate mappings and more

--- AstroFitness/src/main/java/com/astrofitness/bean/Trainer.java
+++ AstroFitness/src/main/java/com/astrofitness/bean/Trainer.java
@@ -1,5 +1,77 @@
 package com.astrofitness.bean;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Trainer")
 public class Trainer {
+	
+	@Id
+	@Column
+	@SequenceGenerator(sequenceName="TRAINER_SEQ", name="TRAINER_SEQ")
+	@GeneratedValue(generator="TRAINER_SEQ", strategy=GenerationType.SEQUENCE)
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private String email;
+	@Column
+	private String password;
+	
+//	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+//	@JoinColumn(name="TRAINER")
+//	@Column
+//	private Gym home_gym;
+	
+	public Trainer() {
+		super();
+	}
+	public Trainer(String name, String email, String password) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	public Trainer(String name, String email, String password, int id) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
