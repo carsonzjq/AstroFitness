a1cc1f644bf74e1520c991c3bae358b019563194
Petar <zigic@outlook.com>
2018-03-08 09:14:03 -0500

    Added Forum, ForumDao and TestForum. Added testng.xml

--- AstroFitness/src/main/java/tester/test.java
+++ AstroFitness/src/main/java/tester/test.java
@@ -1,22 +1,21 @@
 package tester;
 
-import java.util.List;
+import java.sql.Date;
+import java.time.LocalDate;
 
-import com.astrofitness.bean.Gym;
-import com.astrofitness.bean.Trainer;
-import com.astrofitness.dao.GymDao;
-import com.astrofitness.dao.TrainerDao;
+import com.astrofitness.bean.Forum;
+import com.astrofitness.dao.ForumDao;
 
 //Using this class to test hibernate
 public class test {
 	public static void main(String[] args) {
-		GymDao gDao = new GymDao();
+	/*	GymDao gDao = new GymDao();
 		Gym testGym = new Gym();
 		testGym.setId(50);
 		List<Trainer> trainers = gDao.getAllTrainersFromGym(testGym);
 		String buffer = new String();
 		buffer.substring(1);
-		/*		Gym gym1 = new Gym("Planet Gym");
+		Gym gym1 = new Gym("Planet Gym");
 		Gym gym2 = new Gym("LA AstroGym");
 		
 		GymDao gDao = new GymDao();
@@ -25,7 +24,18 @@ public class test {
 		
 		TrainerDao tDao = new TrainerDao();
 		Integer t1 = tDao.insertTrainer(new Trainer("newName", "newEmail","passwordHere",gym1));
-		Integer t2 = tDao.insertTrainer(new Trainer("newName2", "newEmail2","passwordHere2",gym1));
-*/		
+		Integer t2 = tDao.insertTrainer(new Trainer("newName2", "newEmail2","passwordHere2",gym1));*/
+		ForumDao fDao = new ForumDao();
+		Date date1 = Date.valueOf(LocalDate.now());
+		Date date2 = Date.valueOf(LocalDate.now().plusDays(1));	
+		Date date3 = Date.valueOf(LocalDate.now().plusWeeks(1));		  
+		
+		Forum record1 = new Forum("comment 1");
+		Forum record2 = new Forum("comment 2");
+		Forum record3 = new Forum("comment 3");
+		fDao.insertForum(record1);
+		fDao.insertForum(record2);
+		fDao.insertForum(record3);	
+		
 	}
 }
